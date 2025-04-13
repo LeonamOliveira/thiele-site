d5e2b40d635ccbbf77502693d2f5b594ab8ff719
LeonamOliveira <oliveira.leonam@hotmail.com>
2025-04-12 23:15:17 -0300

    feat: finish

--- src/components/feature/machines-features.tsx
+++ src/components/feature/machines-features.tsx
@@ -24,6 +24,43 @@ import image9TH700 from '../../../public/assets/TH700/TH700 - 9.png';
 import image10TH700 from '../../../public/assets/TH700/TH700 - 10.png';
 import image11TH700 from '../../../public/assets/TH700/TH700 - 11.png';
 
+import image1TH800 from '../../../public/assets/TH800/TH800 - 1.png';
+import image2TH800 from '../../../public/assets/TH800/TH800 - 2.png';
+import image3TH800 from '../../../public/assets/TH800/TH800 - 3.png';
+import image4TH800 from '../../../public/assets/TH800/TH800 - 4.png';
+import image5TH800 from '../../../public/assets/TH800/TH800 - 5.png';
+import image6TH800 from '../../../public/assets/TH800/TH800 - 6.png';
+import image7TH800 from '../../../public/assets/TH800/TH800 - 7.png';
+import image8TH800 from '../../../public/assets/TH800/TH800 - 8.png';
+
+import image1TH900 from '../../../public/assets/TH900/TH900 - 1.png';
+import image2TH900 from '../../../public/assets/TH900/TH900 - 2.png';
+import image3TH900 from '../../../public/assets/TH900/TH900 - 3.png';
+import image4TH900 from '../../../public/assets/TH900/TH900 - 4.png';
+import image5TH900 from '../../../public/assets/TH900/TH900 - 5.png';
+
+import image1TH1000 from '../../../public/assets/TH1000/TH1000 - 1.png';
+import image2TH1000 from '../../../public/assets/TH1000/TH1000 - 2.png';
+import image3TH1000 from '../../../public/assets/TH1000/TH1000 - 3.png';
+import image4TH1000 from '../../../public/assets/TH1000/TH1000 - 4.png';
+import image5TH1000 from '../../../public/assets/TH1000/TH1000 - 5.png';
+import image6TH1000 from '../../../public/assets/TH1000/TH1000 - 6.png';
+import image7TH1000 from '../../../public/assets/TH1000/TH1000 - 7.png';
+
+import extratora1 from '../../../public/assets/extratora/extratora - 1.png';
+import extratora2 from '../../../public/assets/extratora/extratora - 2.png';
+import extratora3 from '../../../public/assets/extratora/extratora - 3.png';
+import extratora4 from '../../../public/assets/extratora/extratora - 4.png';
+import extratora5 from '../../../public/assets/extratora/extratora - 5.png';
+import extratora6 from '../../../public/assets/extratora/extratora - 6.png';
+import extratora7 from '../../../public/assets/extratora/extratora - 7.png';
+import extratora8 from '../../../public/assets/extratora/extratora - 8.png';
+
+import polidora1 from '../../../public/assets/polidora/polidora - 1.png';
+import polidora2 from '../../../public/assets/polidora/polidora - 2.png';
+import polidora3 from '../../../public/assets/polidora/polidora - 3.png';
+import polidora4 from '../../../public/assets/polidora/polidora - 4.png';
+import polidora5 from '../../../public/assets/polidora/polidora - 5.png';
 
 import imageTH310 from "../../../public/assets/TH310.png";
 import imageTH800 from "../../../public/assets/TH800 - 1.png";
@@ -159,6 +196,16 @@ export const featureMachineText: FeatureText[] = [
     capacity: "Tanques de 90 | 100 litros",
     productivity: "Produtividade de 3000 m²/h",
     battery: "Autonomia de 3 à 4 horas",
+    carousel: [
+      image1TH800,
+      image2TH800,
+      image3TH800,
+      image4TH800,
+      image5TH800,
+      image6TH800,
+      image7TH800,
+      image8TH800
+    ],
     specifications: [
       "Tanques de Solução/Recuperação: 85/90 L",
       "Largura de Limpeza: 900 mm",
@@ -181,6 +228,13 @@ export const featureMachineText: FeatureText[] = [
     capacity: "Tanques de 120|130 litros",
     productivity: "Produtividade de 3500 m²/h",
     battery: "Autonomia de 3 à 4 horas",
+    carousel: [
+      image1TH900,
+      image2TH900,
+      image3TH900,
+      image4TH900,
+      image5TH900
+    ],
     specifications: [
       "Tanques de Solução/Recuperação: 115/125 L",
       "Largura de Limpeza: 1020 mm",
@@ -203,6 +257,15 @@ export const featureMachineText: FeatureText[] = [
     capacity: "Tanques de 155 | 170 litros",
     productivity: "Produtividade de 4500 m²/h",
     battery: "Autonomia de 3 à 4 horas",
+    carousel: [
+      image1TH1000,
+      image2TH1000,
+      image3TH1000,
+      image4TH1000,
+      image5TH1000,
+      image6TH1000,
+      image7TH1000,
+    ],
     specifications: [
       "Tanques de Solução/Recuperação: 150/165 L",
       "Largura de Limpeza: 1200 mm",
@@ -268,6 +331,16 @@ export const featureMachineText: FeatureText[] = [
     capacity: "Motor: 15hp",
     productivity: "Consumo de Álcool: 1,9L/h",
     battery: "Elétrica, à bateria (12V)",
+    carousel: [
+      extratora1,
+      extratora2,
+      extratora3,
+      extratora4,
+      extratora5,
+      extratora6,
+      extratora7,
+      extratora8
+    ],
     specifications: [
       "Tanques de Solução/Recuperação: 390/400 L",
       "Largura de Limpeza: 1380 mm",
@@ -289,6 +362,13 @@ export const featureMachineText: FeatureText[] = [
     capacity: "Motor: 16hp",
     productivity: "Consumo de Álcool: 1,9l/h",
     battery: "Autonomia de 6 à 8 horas",
+    carousel: [
+      polidora1,
+      polidora2,
+      polidora3,
+      polidora4,
+      polidora5 
+    ],
     specifications: [
       "Motor: 15 HP",
       "Combustível: Etanol",
