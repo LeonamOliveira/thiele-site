67a806cb66f718290db13868414079c4a4bebf86
LeonamOliveira <oliveira.leonam@hotmail.com>
2025-04-25 10:13:36 -0300

    feat: change informations

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -48,15 +48,11 @@ export default function Header() {
             initial={{ opacity: 0, y: -10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.4 }}
-            className="text-center md:text-right hover:scale-105 transition-transform duration-300 text-sm"
+            className="text-center md:text-right hover:scale-105 transition-transform duration-300"
           >
-            <strong>
-              <a href="http://wa.me/+5511930111440?text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es" className="whitespace-nowrap">
-                Central de atendimento: (11) 93011-1440
-              </a>
-            </strong>
-            <br/>
-            <strong className="whitespace-nowrap">comercial@thiele.com.br</strong>
+            <strong><a href="http://wa.me/+5511930111440?text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es">Central de atendimento: (11) 93011-1440</a></strong>
+            <br />
+            <strong>comercial@thiele.com.br</strong>
           </motion.div>
         </div>
       </div>
